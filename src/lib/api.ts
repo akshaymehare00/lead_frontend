e588d78f8dbe7313eafd23c532ab4dd10725dd77
akshaymehare00 <akshaymehare2300@gmail.com>
2026-03-18 21:42:18 +0530

    urls changes

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,8 +6,8 @@
 // const API_BASE =
 //   import.meta.env.VITE_API_URL ||
 //   (import.meta.env.PROD ? "https://lead-backend-1-kcve.onrender.com" : "http://localhost:3000");
-const API_BASE = "https://lead-backend-exp.onrender.com";
-// const API_BASE = "http://localhost:3000";
+// const API_BASE = "https://lead-backend-exp.onrender.com";
+const API_BASE = "http://localhost:3000";
 
 const TOKEN_KEY = "lead-compass-token";
 
